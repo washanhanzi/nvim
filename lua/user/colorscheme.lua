e1f21a900981aa17b24cdcfa9a6baebb4738ab5a
franc <francismajere@gmail.com>
2022-10-28 16:25:23 +0800

    change color theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,15 @@
-local colorscheme = "tokyonight-night"
+local colorscheme = "onedarkpro"
+
+require("onedarkpro").setup({
+	dark_theme = "onedark_dark",
+	colors = {
+		onedark_dark = {
+			comment = "#0ffc03",
+		},
+	},
+})
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
