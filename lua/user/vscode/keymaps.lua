b855c948407c17dcf4c28aece4658e10b61623d9
franc <francismajere@gmail.com>
2023-01-05 13:22:13 +0800

    FEAT add some comments

--- lua/user/vscode/keymaps.lua
+++ lua/user/vscode/keymaps.lua
@@ -3,7 +3,9 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
+-- ctrl+y to copy to system clipboard
 keymap("v", "<C-y>", '"+y', opts)
+-- ctrl+shift+y to copy the whole file to system clipboard
 keymap("n", "<C-S-y>", 'gg"+yG', opts)
 
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
